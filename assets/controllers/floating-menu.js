6a2c3390379bdc214e7fa3a7bfe6c4aea8096887
SofiDevO <angiemeow89@gmail.com>
2023-08-08 17:12:36 -0600

    Seccion login terminada

--- assets/controllers/floating-menu.js
+++ assets/controllers/floating-menu.js
@@ -33,7 +33,7 @@ window.addEventListener("scroll", (e) => {
 
 const toTop = d.querySelector(".to__top") 
 window.addEventListener("scroll",()=>{
-	if(window.pageYOffset > 2150){
+	if(window.pageYOffset > 900){
 		toTop.classList.add("active");
 	}else{
 		toTop.classList.remove("active");
