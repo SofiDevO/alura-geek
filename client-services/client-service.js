6c09945263206e8c31274a6a28a611e3167e4c78
SofiDevO <angiemeow89@gmail.com>
2023-08-12 19:28:28 -0600

    Terminado el disenho

--- client-services/client-service.js
+++ client-services/client-service.js
@@ -1,3 +1,14 @@
+const d = document;
 
+const productosAll = () =>
+	fetch("http://127.0.0.1:3000/producto").then((respuesta) => respuesta.json());
 
-
+    const crearproducto = (url, name, price, description) => {
+        return fetch("http://127.0.0.1:3000/producto", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ url, name, price, description: uuid.v4() }),
+        });
+    };
